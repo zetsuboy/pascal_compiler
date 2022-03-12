e6a268e4dd2f6edc5d51d1587f164c0aee1a96b7
ada0l <andreika.varfolomeev@yandex.ru>
2022-03-12 11:23:54 +1000

    init

--- HW1/HW1/HW1/MainPage.xaml.cs
+++ HW1/HW1/HW1/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,39 @@ using Xamarin.Forms;
 
 namespace HW1
 {
+    public class Item
+    {
+        public Item(string item_name, int item_count)
+        {
+            this.item_name = item_name;
+            this.item_count = item_count;
+        }
+
+        public string item_name
+        {
+            get; set;
+        }
+        
+        public int item_count
+        {
+            get; set;
+        }
+    }
+
     public partial class MainPage : ContentPage
     {
+        public static ObservableCollection<Item> items = new ObservableCollection<Item>();
+
         public MainPage()
         {
             InitializeComponent();
+            
+            BindableLayout.SetItemsSource(items_container, items);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AddItemPage());
         }
     }
 }
