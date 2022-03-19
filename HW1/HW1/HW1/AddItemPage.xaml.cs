72b4f6ca00dc3326676e836279c619ca87df279a
Jeskay <37577958+Jeskay@users.noreply.github.com>
2022-03-19 16:43:21 +1000

    Count parse fixed, code refactored

--- HW1/HW1/HW1/AddItemPage.xaml.cs
+++ HW1/HW1/HW1/AddItemPage.xaml.cs
@@ -12,7 +12,17 @@ namespace HW1
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddItemPage : ContentPage
     {
-        private static int id = 0;
+        private const int max_limit = 100;
+        private const int min_limit = 1;
+        public struct SelectedItem
+        {
+            public string Name;
+            public int Count;
+        }
+        public SelectedItem selectedItem 
+        {
+            get; private set;
+        }
         public AddItemPage()
         {
             InitializeComponent();
@@ -21,22 +31,22 @@ namespace HW1
                 "Baikal",
                 "Coca-cola"
             };
+            drinks.SelectedIndex = 0;
         }
 
 
         private void IncreaseItem(string name, int count = 1)
         {
-            MainPage.items.Where(x => x.item_name == name).ToList().ForEach(x =>
-            {
-                count += x.item_count;
-                MainPage.items.Remove(x);
-            });
-            MainPage.items.Add(new Item(name, count, id++));
+            selectedItem = new SelectedItem() { 
+                Name = name, 
+                Count = count 
+            };
         }
 
         private void Button_Order(object sender, EventArgs e)
         {
             IncreaseItem(drinks.SelectedItem.ToString(), Int32.Parse(count.Text));
+            Navigation.PopAsync();
         }
 
         private void Button_Minus(object sender, EventArgs e)
@@ -47,6 +57,31 @@ namespace HW1
         private void Button_Plus(object sender, EventArgs e)
         {
             count.Text = ((Int32.Parse(count.Text)) + 1).ToString();
+        }
+
+        private void count_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (UInt32.TryParse(e.NewTextValue, out var number))
+            {
+                if (number > max_limit)
+                {
+                    count.Text = max_limit.ToString();
+                }
+                else if (number < min_limit)
+                {
+                    count.Text = min_limit.ToString();
+                }
+                else 
+                {
+                    count.Text = number.ToString();
+                }
+                
+            }
+            else 
+            {
+                count.Text = e.OldTextValue;
+            }
+            
         }
     }
 }
