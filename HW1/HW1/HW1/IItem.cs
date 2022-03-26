e87a6c27aeb6d75636ad1a8c9eef286fc4bae68d
ada0l <andreika.varfolomeev@yandex.ru>
2022-03-26 11:23:47 +1000

    Change xaml and add description

---
+++ HW1/HW1/HW1/IItem.cs
@@ -0,0 +1,12 @@
+﻿namespace HW1
+{
+    public interface IItem
+    {
+        int item_count { get; set; }
+        int item_id { get; }
+        string item_image { get; set; }
+        string item_name { get; set; }
+
+        void UpdateCount(int newCount);
+    }
+}
